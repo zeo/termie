a91fc5ff0e35baac0ebe0da37ed084d3bf44decd
rot <96458554+rotlove@users.noreply.github.com>
2026-05-30 18:58:52 +0300

    chore: stop tracking plugin build artifacts; gitignore plugins/*/target
    the phase-2 commit accidentally included plugins/tamagotchi/target (compiled
    exe/pdb/fingerprints) because the gitignore rule landed late. drop them from
    tracking and ignore nested plugin target dirs.

--- plugins/tamagotchi/target/release/deps/tamagotchi.d
+++
@@ -1,5 +0,0 @@
-D:\Applications\fJune\termie\plugins\tamagotchi\target\release\deps\tamagotchi.d: main.rs
-
-D:\Applications\fJune\termie\plugins\tamagotchi\target\release\deps\tamagotchi.exe: main.rs
-
-main.rs:
--- plugins/tamagotchi/target/release/tamagotchi.d
+++
@@ -1,1 +0,0 @@
-D:\Applications\fJune\termie\plugins\tamagotchi\target\release\tamagotchi.exe: D:\Applications\fJune\termie\plugins\tamagotchi\main.rs
